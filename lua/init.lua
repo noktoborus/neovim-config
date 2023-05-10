186672fb5aeb37c10c92c763f3fe66e6f74661f5
Noktoborus <noktoborus@gmail.com>
2023-05-11 01:49:00 +0400

    update lspsaga initialization to new version

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
     -- one of "all", "maintained" (parsers with maintainers),
     -- or a list of languages
-    ensure_installed = { "c", "yaml", "rust", "vim", "json", "meson", "markdown" },
+    ensure_installed = { "c", "yaml", "rust", "vim", "json", "meson", "markdown", "markdown_inline" },
 
     highlight = {
         enable = true,
@@ -222,47 +222,26 @@ require('winbar').setup({
     --  },
 })
 
-require('lspsaga').init_lsp_saga({
-    preview_lines_above = 2,
-    max_preview_lines = 10,
-    show_outline = {
-        win_position = 'left',
-        --set special filetype win that outline window split.like NvimTree neotree
-        -- defx, db_ui
-        win_with = '',
-        win_width = 30,
-        auto_enter = true,
-        auto_preview = true,
-        virt_text = '┃',
-        jump_key = '<CR>',
-        -- auto refresh when change buffer
-        auto_refresh = true,
+require('lspsaga').setup({
+    diagnostic = {
+        keys = {
+            quit = '<Esc>',
+            quit_in_show = { 'q', '<ESC>' },
+        },
     },
-    code_action_lightbulb = {
-        enable = true,
-        enable_in_insert = true,
-        cache_code_action = true,
-        sign = true,
-        update_time = 150,
-        sign_priority = 20,
-        virtual_text = true,
+    rename = {
+        quit = "<Esc>",
     },
-    code_action_keys = {
-        quit = '<Esc>',
-        exec = '<CR>',
+    finder = {
+        keys = {
+            expand_or_jump = '<CR>',
+        },
     },
-    rename_action_quit = '<Esc>',
-    symbol_in_winbar = {
-        in_custom = false,
-        enable = true,
-        --    separator = ' > ',
-        show_file = true,
-        -- define how to customize filename, eg: %:., %
-        -- if not set, use default value `%:t`
-        -- more information see `vim.fn.expand` or `expand`
-        -- ## only valid after set `show_file = true`
-        file_formatter = "",
-        click_support = false,
+    outline = {
+        keys = {
+            expand_or_jump = '<CR>',
+            quit = "<ESC>",
+        },
     },
 })
 
